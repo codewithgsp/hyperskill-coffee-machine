5379663c9bb45b6e6fa83519f1ad3c15a51fdb08
Saiprasadgorre <dare2dream30@gmail.com>
2020-06-01 20:54:34 +0200

    Hexagon area

---
+++ Problems/Hexagon/task.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+
+class Hexagon:
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    # create get_area here
+    def get_area(self):
+        return round(3 * sqrt(3) * (self.side_length ** 2) * 0.5, 3)
