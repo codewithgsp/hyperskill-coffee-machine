ae9dbf2f5efd15540f78c8d6d7f7d27794249506
Saiprasadgorre <dare2dream30@gmail.com>
2020-06-03 22:59:57 +0200

    __str__ vs __repr__ -> comments

--- Problems/Magic methods/str_vs_repr.py
+++ Problems/Magic methods/str_vs_repr.py
@@ -14,3 +14,9 @@ class Transactions:
 
 payment = Transactions('00001', 3490.0)
 print(payment)
+
+# __repr__ is more for debugging and for developers,
+# __str__ is more for users
+
+# if both __repr__ and __str__ are used, __str__ will be picked for print
+# if __str__ not present, __repr__ will be called automatically
